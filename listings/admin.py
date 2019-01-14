5f015eb6f46a80173e4a2c27fdd9f16116ff624a
Arwash <optimist4ever90@gmail.com>
2019-01-14 17:37:09 +0300

    Cutomizing the admin pages of realtors and listings by adding data tables

--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from .models import listing
 # Register your models here.
 
-admin.site.register(listing)
+# cutomizing what appears in the listings page in the admin area
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+admin.site.register(listing, ListingAdmin)
