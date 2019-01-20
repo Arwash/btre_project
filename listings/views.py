6d044d6e1f0b8036821225f1c02b90262886443e
Arwash <optimist4ever90@gmail.com>
2019-01-20 14:59:05 +0300

    Finishing the single listing page

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 from .models import listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import bedroom_choices, state_choices, price_choices
 # Create your views here.
 def index(request):
     #it was listings = listing.objects.all() but we change it to the current method to fetch the objects ordered descently by time
     # the filter is to view the objects that are marked in the admin area as published
     listings = listing.objects.order_by('-list_date').filter(is_published=True)  
     
+
     #Pagination code
     paginator = Paginator(listings, 6) #The number is number of items per page
     page = request.GET.get('page')
@@ -16,10 +18,8 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def Listing(request, listing_id):
-    listing = get_object_or_404(listing, pk=listing_id) #the urgument Listing is the model, we do get object or 404 because if a user entered the link for an object thatdoes not exiist we want ot show it
-
-    context = {'listing':listing}
-
+    Listing = get_object_or_404(listing, pk=listing_id) #the urgument Listing is the model, we do get object or 404 because if a user entered the link for an object thatdoes not exiist we want ot show it
+    context = {'listing': Listing}
     return render(request, 'listings/listing.html', context)
 
 def search(request):
